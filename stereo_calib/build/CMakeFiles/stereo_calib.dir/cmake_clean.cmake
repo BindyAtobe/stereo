5f343f3c648571a8df6f2064549d36d98accfb8e
BindyAtobe <919818192@qq.com>
2019-11-25 13:47:29 +0800

    Add files via upload

---
+++ stereo_calib/build/CMakeFiles/stereo_calib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stereo_calib.dir/main.cpp.o"
+  "stereo_calib"
+  "stereo_calib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stereo_calib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
